d8d4d095a49b05de15711198ba811c60922c873f
Srijan0405 <srijanhardik16@gmail.com>
2026-05-04 15:36:25 +0530

    [Srijan] Pushed Practical VIva

---
+++ Practical-Viva/middleware/authmiddleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "Token not provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user;
+        next();
+
+    } catch (err) {
+        return res.status(401).json({ message: "Token is not valid" });
+    }
+};
